ad85e529ec9f9840c34e3e3c6fe742ea3eae0be1
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2021-11-18 22:49:41 +0200

    fix head

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ import Footer from '../src/Components/Footer';
 import Formation from '../src/Components/Formation';
 import Header from '../src/Components/Header';
 import Main from '../src/Components/Main';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>kkphotographer Portifolio</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Header />
       <Main />
       <Experience />
